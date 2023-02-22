40f0042f3086d9754d4884c34eb5eef1ed4d1f1c
j-nishime <j_nishime@codmon.co.jp>
2023-02-22 11:43:31 +0900

    feat: とりあえず雑にテストを通す

--- src/Main.php
+++ src/Main.php
@@ -12,14 +12,50 @@ class Main
      * 処理の開始地点
      *
      * @param array $coins 投入額
-     * @param string $menu 注文
+     * @param string $order 注文
      * @return string おつり。大きな硬貨順に枚数を並べる。なしの場合はnochange
      * ex.)
      * - 100円3枚、50円1枚、10円3枚なら"100 3 50 1 10 3"
      */
-    public static function runSimply(array $coins, string $menu): string
+    public static function runSimply(array $coins, string $order): string
     {
-        return "do implementation";
+        $menu = array(
+            'cola' => 120,
+            'coffee' => 150,
+            'energy_drink' => 210
+        );
+        $price = $menu[$order];
+        $money = self::getTotalMoney($coins);
+        $change = $money - $price;
+        return self::getChangeToString($change);
+    }
+
+    public static function getTotalMoney(array $coins): int {
+        $money = 0;
+        foreach($coins as $coin => $amount) {
+            $money += intval($coin) * $amount;
+        }
+        return $money;
+    }
+
+    public static function getChangeToString(int $change): string {
+        if ($change === 0) {
+            return "nochange";
+        }
+        $changeCoins = array(500, 100, 50, 10);
+        $changeToString = "";
+        foreach($changeCoins as $coin){
+            $res = $change / $coin;
+            $amount = floor($res);
+            if ($amount > 0) {
+                if ($changeToString !== ""){
+                    $changeToString .= " ";
+                }
+                $changeToString .= "{$coin} {$amount}";
+                $change = $change - ($coin * $amount);
+            }
+        }
+        return $changeToString;
     }
 
     /**
